a906229a1e00daf08e28b91462d088e0ac287403
Himanshu <sagarhimanshu472@gmail.com>
2025-01-03 23:03:31 +0530

    feat: uploading and generating pdf

--- backend/routes/pdf.js
+++ backend/routes/pdf.js
@@ -1,16 +1,11 @@
 const express = require('express')
 const  PDFDocument = require('pdfkit')
-const multer = require('multer');
-const { extractTextFromDocument } = require('../utils/documentProcessor');
-const { summarizeTextInChunks, cleanSummary } = require('../utils/summarization');
 
 const router = express.Router();
-const storage = multer.memoryStorage(); 
-const upload = multer({ storage });
 
 router.post('/generate-pdf', (req, res) => {
     const { summary, theme } = req.body;
-
+    
     if (!summary) {
         return res.status(400).send('Summary is required');
     }
