a61bc7f258d43c5b6d06cd03ea94f8ea87241721
Hi <sagarhimanshu472@gmail.com>
2025-02-01 16:19:01 +0530

    feat: transcript grpc server setup complete

---
+++ backend/summarizer/summarizer.go
@@ -0,0 +1,37 @@
+package main 
+
+import (
+    "context"
+    "net"
+    "log"
+
+    "google.golang.org/grpc"
+    "github.com/Himanshuu23/yt-video-summarizer/backend/summarizer"
+)
+
+type mySummarizerServer struct {
+    summarizer.UnimplementedSummarizerServer
+}
+
+func (s mySummarizerServer) GetTranscript(context.Context, *transcriber.TranscriptRequest) (*TranscriptResponse, error) {
+   return &transcriber.TranscriptResponse{
+	transcript: "this are the transcript for the require youtube video"
+   }, nil 
+}
+
+func main() {
+    list, err := net.Listen( network: "tcp", address: ":8089")
+    if err != nil {
+	log.Fatalf(v...."cannot create listener: %s", err)
+    }
+
+    serverRegistrar := grpc.NewServer()
+    service := &myTranscriberServer{}
+
+    transcriber.RegisterTranscriberServer(serverRegistrar)
+    
+    err := serverRegistrar.Serve(list)
+    if err != nil {
+	log.Fatalf(format: "Impossible to serve: %s", err)
+    }
+}
