c9232c112f7cd7c4a3ef4d573c40b86fde9a3109
Hi <sagarhimanshu472@gmail.com>
2025-02-01 16:20:35 +0530

    feat: summarizer grpc server basic setup

--- Youtube-Video-Summarizer/app/components/HomePageComponents/Hero.tsx
+++ Youtube-Video-Summarizer/app/components/HomePageComponents/Hero.tsx
@@ -22,7 +22,7 @@ export default function Hero() {
       
           <div className="relative z-10 max-w-4xl px-4">
             <h1 className="text-3xl md:text-5xl font-bold mb-4">One-click and Summarized!</h1>
-            <p className="text-base md:text-lg mb-8">Dive into the art assets, where innovative blockchain technology meets financial expertise.</p>
+            <p className="text-base md:text-lg mb-8">Transform YouTube videos, lengthy PDFs, and complex documents into clear, engaging notes with multiple themes and language support.</p>
             <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4 justify-center">
               <a href="#" className="px-6 py-3 bg-black/40 hover:bg-black/60 rounded-full text-sm">Open App</a>
               <a href="#" className="px-6 py-3 bg-white text-black hover:bg-gray-200 rounded-full text-sm">Discover More</a>
--- backend/transcriber/transcriber.go
+++ backend/transcriber/transcriber.go
@@ -1,4 +1,37 @@
-import "net"
+package main 
+
+import (
+    "context"
+    "net"
+    "log"
+
+    "google.golang.org/grpc"
+    "github.com/Himanshuu23/yt-video-summarizer/backend/transcriber"
+)
+
+type myTranscriberServer struct {
+    transcriber.UnimplementedTranscriberServer
+}
+
+func (s myTranscriberServer) GetTranscript(context.Context, *transcriber.TranscriptRequest) (*TranscriptResponse, error) {
+   return &transcriber.TranscriptResponse{
+	transcript: "this are the transcript for the require youtube video"
+   }, nil 
+}
 
 func main() {
-    net.Listen()
+    list, err := net.Listen( network: "tcp", address: ":8089")
+    if err != nil {
+	log.Fatalf(v...."cannot create listener: %s", err)
+    }
+
+    serverRegistrar := grpc.NewServer()
+    service := &myTranscriberServer{}
+
+    transcriber.RegisterTranscriberServer(serverRegistrar)
+    
+    err := serverRegistrar.Serve(list)
+    if err != nil {
+	log.Fatalf(format: "Impossible to serve: %s", err)
+    }
+}
