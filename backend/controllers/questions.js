2a73896ae8ada151db76575a0f7184f68628da1c
Himanshu <sagarhimanshu472@gmail.com>
2025-03-09 20:31:31 +0530

    feat: generating questions from the text

--- backend/controllers/questions.js
+++ backend/controllers/questions.js
@@ -1,5 +1,18 @@
+require('dotenv').config();
+
+const API_KEY = process.env.BART_API_KEY;
+const API_URL = process.env.BART_API_URL_TWO;
+
 const generateQuestions = async (req, res) => {
-    return res.json({ res: "this is the response for the same for the testing purposes" })
+    const response = await fetch(`${API_URL}`, {
+        method: "POST",
+        headers: { "Authorization": `Bearer ${API_KEY}` },
+        body: `Generate 10 questions **with answers** from : ${req.body.data}`
+    })
+
+    const data = await response.json()
+    console.log(data)
+    return res.json({ res: data })
 }
 
 module.exports = { generateQuestions };
--- backend/routes/summary.js
+++ backend/routes/summary.js
@@ -17,9 +17,6 @@ router.post('/file', upload.single('file'), async (req, res) => {
 module.exports = router;
 
 {/*
-1. Refactor the code and test the new endpoints using postman
-2. Do the translation part - from the frontend 
-3. Also since by default the language would be english maybe check if the model accuracies can be corrected if we retranslate the text in english using the google api
 4. The pdf should look much better (include the flowchart, question, images, formulae etc as mentioned in the notepad file)   
 5. In the demo use static data for maths formulae and summary and images - to fool the people for the same and then later excuse that it was when i was using amazing api but now its limit is reached so using the bad one now
 6. Payments using RazorPay
