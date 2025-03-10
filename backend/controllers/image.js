d6ca3c7344d21dc299c817f8ff18e0a1b0cd8b82
Himanshu <sagarhimanshu472@gmail.com>
2025-03-10 18:17:10 +0530

    feat: generate-image endpoint complete

--- backend/controllers/image.js
+++ backend/controllers/image.js
@@ -1,40 +1,35 @@
+require('dotenv').config()
+
 const generateImage = async (req, res) => {
-    return res.json({ res: "this is the response from the generate-image route" })
+    
+    if (!req.body.prompt) {
+        return res.status(400).json({ error: "No Prompt Found!" })
+    }
+
+    try {
+        const response = await fetch('https://api.imagepig.com', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Api-Key': `${process.env.IMAGE_PIG_API_KEY}`
+            },
+            body: JSON.stringify({ "prompt": req.body.prompt })
+        })
+
+        if (!response.ok) {
+            errors = await response.text()
+            console.log(errors)
+            return res.status(response.status).json({ "error": "Failed to fetch image" })
+        }
+
+        const json = await response.json()
+        const buffer = Buffer.from(json.image_data, 'base64')
+
+        res.setHeader('Content-Type', 'image/jpeg')
+        res.send(buffer)
+    } catch (error) {
+        res.status(500).json({ error: error.message })
+    }
 }
 
 module.exports = { generateImage }
-
-// const fs = require('node:fs');
-// const { Buffer } = require('node:buffer');
-
-// void async function() {
-//     try {
-//         response = await fetch('https://api.imagepig.com/', {
-//             method: 'POST',
-//             headers: {
-//                 'Content-Type': 'application/json',
-//                 'Api-Key': 'your-api-key'
-//             },
-//             body: JSON.stringify({"prompt": "adorable pig"})
-//         });
-
-//         if (!response.ok) {
-//             throw new Error(`Response status: ${response.status}`);
-//         }
-
-//         json = await response.json();
-//         const buffer = Buffer.from(json.image_data, 'base64');
-
-//         fs.writeFile(
-//             'adorable-pig.jpeg',
-//             buffer,
-//             (error) => {
-//                 if (error) {
-//                     throw error;
-//                 }
-//             }
-//         );
-//     } catch (error) {
-//         console.error(error);
-//     }
-// }();
