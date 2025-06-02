9ebd0c6e38ce681bc1150b8800c922be37dcf3be
Himanshu <sagarhimanshu472@gmail.com>
2025-06-02 23:01:47 +0530

    feat: initialized each route with /api

--- backend/index.js
+++ backend/index.js
@@ -11,10 +11,10 @@ const PORT = 8000
 
 app.use(cors());
 app.use(express.json());
-app.use('/user', userRouter)
-app.use('/summarize', summaryRouter)
-app.use('/pdf', pdfRouter)
-app.use('/translate', translateRouter)
+app.use('/api/user', userRouter)
+app.use('/api/summarize', summaryRouter)
+app.use('/api/pdf', pdfRouter)
+app.use('/api/translate', translateRouter)
 
 app.listen(PORT, () => {
     console.log(`Server Started at PORT: ${PORT}`)
